0af954050e6cdcfdf298e994835ce12c5fa3daff
indraanjay <indrakampus01@gmail.com>
2026-01-03 10:49:12 +0800

    menyelesaikan migrasi dan model

---
+++ database/migrations/2026_01_03_010421_create_detail_pesanans_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('detail_pesanans', function (Blueprint $table) {
+            $table->id('id_detail');
+            $table->foreignId('id_pesanan')->constrained('pesanans', 'id_pesanan')->onDelete('cascade');
+            $table->foreignId('id_produk')->constrained('produks', 'id_produk');
+            $table->integer('jumlah');
+            $table->decimal('subtotal', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_pesanans');
+    }
+};
